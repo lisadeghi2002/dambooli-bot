1123221dcfc32bbc5cf421b5b2cdc72309f670b5
li-sadeghi <a.sadeghiali@yahoo.com>
2025-12-26 15:10:41 +0330

    fix issues

--- main.py
+++ main.py
@@ -95,7 +95,7 @@ async def health_server():
     app = web.Application()
     app.router.add_get("/", handle)
 
-    port = int(os.environ.get("PORT", 8080))
+    port = int(os.environ.get("PORT", 8000))
     runner = web.AppRunner(app)
     await runner.setup()
     site = web.TCPSite(runner, "0.0.0.0", port)
